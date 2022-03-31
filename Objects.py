9900a15b574865fba0e4ead2a4cba377494a96ce
austincarreno <austinraycarreno96@gmail.com>
2022-03-31 18:43:47 -0400

    storing output from clasp with penalty / possibilistic. Currently working on searching list of feasible objects from penalty logic output for feasible objects from hard constraints output

--- Objects.py
+++ Objects.py
@@ -20,30 +20,45 @@ class Constraint:
         self.num = 0
         self.input = ""
         self.output = ""
+        self.penalty = 0
+        self.tolerance = 0
 
 class Penalty:
     def __init__(self):
         self.num = 0
         self.pen = 0
-        self.input = ""
-        self.output = ""
+        self.input_as_words = ""
+        self.input_as_num = ""
+        self.output_as_words = ''
+        self.output_as_num = []
 
-class Penalty_Object:
-    def __init__(self):
-        self.feasible_object = ''
+# class Penalty_Object:
+#     def __init__(self):
+#         self.logic_statement = ''
+#         self.feasible_object_as_num = ''
+#         self.feasible_object = ''       # FixMe: Not sure if this is necessary
+#         self.penalty = 0
+
 
 class Possibilistic:
     def __init__(self):
         self.num = 0
         self.tol = 0
-        self.input = ""
-        self.output = ""
+        self.input_as_words = ''
+        self.input_as_num = ''
+        self.output_as_words = ''
+        self.output_as_num = []
+
+class Possibilistic_Object:
+    def __init__(self):
+        self.feasible_object_as_num = ''
+        self.feasible_object = ''
 
 class Feasible:
     def __init__(self):
         self.name = ""
         self.name_as_num = ''
-        self.pen1 = 0
+        self.penalty_list = []
         self.pen2 = 0
         self.pen3 = 0
         self.pen_total = 0
